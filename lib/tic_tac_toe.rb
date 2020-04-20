f50941952e793643e74fe50a8b109d41a2469cf3
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-20 22:49:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -99,7 +99,7 @@ class TicTacToe
   
   #### `#play`
 
-The play method is the main method of the Tic Tac Toe application and is responsible for the game loop. A Tic Tac Toe game must allow players to take turns, checking if the game is over after every turn. At the conclusion of the game, whether because it was won or ended in a draw, the game should report to the user the outcome of the game. You can imagine the pseudocode:
+#The play method is the main method of the Tic Tac Toe application and is responsible for the game loop. A Tic Tac Toe game must allow players to take turns, checking if the game is over after every turn. At the conclusion of the game, whether because it was won or ended in a draw, the game should report to the user the outcome of the game. You can imagine the pseudocode:
 
 ```
 until the game is over
@@ -112,6 +112,9 @@ else if the game was a draw
   tell the players it ended in a draw
 end
 ```
-
+while !over? do
+  
+  
+end
 
 end
